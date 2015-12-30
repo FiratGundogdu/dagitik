6d6568c78d8502341cdb42ac281a37f30af85099
firatlepirate <firatlepirate@firatlepirate>
2015-12-30 18:45:29 +0200

    Client ve Server threadler geliştirildi. Fakat eksikler hala mevcut :/

--- proje/peer.py
+++ proje/peer.py
@@ -12,6 +12,153 @@ import random
 import ip
 
 
+
+class  ServerThread(threading.Thread):
+     def __init__(self, name, peer_ip, peer_port):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.peer_ip=peer_ip
+        self. peer_port=peer_port
+
+
+     def run(self):
+          peer_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          peer_socket.bind(( self.peer_ip,self. peer_port))
+          peer_socket.listen(5)
+          threadCounter=0
+          while True:
+              cPeer,addrPeer=peer_socket.accept()
+              threadCounter += 1
+              threadPeer = ServerReadThread("ServerReadThread"+str(threadCounter), cPeer, addrPeer)
+              threadPeer.start()
+
+class ServerReadThread (threading.Thread):
+    def __init__(self, name, cSocket, address):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.cSocket = cSocket
+        self.address = address
+        self.peer_ip=""
+        self.peer_port=""
+        self.connect_point={}
+        self.check_command_nummber=""
+        self.msg_list=""
+        self.file_name_list=""
+        self.user_add=""
+        self.running=True
+
+    def parser(self,data) :
+         global connect_point_list
+         global connect_point_temp_list
+
+
+         if data[0:5]=="HELLO" :
+              response = "SALUT"
+              self.cSocket.send(response)
+         elif data[0:5]=="CLOSE":
+              response = "BUBYE"
+              self.cSocket.send(response)
+              self.cSocket.close()
+         elif data[0:5]=="REGME":
+              self.peer_ip=str(data[6:data.index(":")])
+              self.peer_port=data[data.index(":")+1:]
+
+
+              if  self.peer_ip+str(self.peer_port)  in connect_point_temp_list.keys():
+                   response = "REGOK"
+
+                   try:
+                        self.cSocket.send(response)
+                   except socket.error:
+                        self.cSocket.close()
+                   self.check_command_nummber=2
+
+              else:
+                   t=time.ctime()
+                   self.connect_point["time"]=t
+                   self.connect_point["peer_port"]= self.peer_port
+                   self.connect_point["peer_ip"]=self.peer_ip
+                   self.user_add=self.peer_ip+":"+str(self.peer_port)+":"+str(t)
+                   connect_point_list.append(self.user_add)
+                   connect_point_temp_list[self.peer_ip+str(self.peer_port)]="True"
+                   try:
+                        self.cSocket.send("REGWA")
+                   except socket.error:
+                        self.cSocket.close()
+                   time.sleep(0.4)
+                   try:
+                        self.cSocket.send("REGOK "+str(t))
+                   except socket.error:
+                        self.cSocket.close()
+                   self.check_command_nummber=1
+                   self.cSocket.close()
+              print(connect_point_list)
+
+         elif data[0:5]=="GETNL":
+              if len(data)>5:
+                   if len(connect_point_list)>5:
+                        size_list=5
+                   else:
+                        size_list=len(connect_point_list)
+
+              else:
+                   size_list=len(connect_point_list)
+
+              for  user  in  connect_point_list[0: size_list] :
+                   self.msg_list=self.msg_list+user+"\n"
+              self.cSocket.send("NLIST BEGIN"+"\n"+self.msg_list+"NLIST END")
+              self.cSocket.close()
+              print("NLIST BEGIN"+"\n"+self.msg_list+"NLIST END")
+              print(self.msg_list)
+              print("Listeyi gonderdim")
+
+         elif data[0:11]=="NLIST BEGIN":
+              print("GELEN"+data)
+              connect_point_list=[]
+              connect_point_list=data.split("\n")
+              connect_point_list=connect_point_list[1:-1]
+              print(connect_point_list)
+              connect_point_temp_list.clear()
+              for  i  in connect_point_list:
+                   tempdiz=i.split(":")
+                   connect_point_temp_list[tempdiz[0]+tempdiz[1]]="True"
+              self.cSocket.close()
+              print("Guncel listeyi aldim")
+
+         else:
+              response = "REGER"
+              try:
+                   self.cSocket.send(response)
+                   self.cSocket.close()
+              except socket.error:
+                   self.cSocket.close()
+
+
+
+    def run(self):
+         while self.running:
+              try:
+                    incoming_data=self.cSocket.recv()
+
+              except socket.error ,e:
+                   err=e.args[0]
+                   if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                      time.sleep(1)
+                      print 'No data available'
+                      continue
+              self.parser(incoming_data)
+
+              if incoming_data=="CLOSE":
+                   break
+              if incoming_data[0:5]=="REGME":
+                   if   self.check_command_nummber==1:
+                        break
+              elif incoming_data[0:5]=="GETNL":
+                   if   self.check_command_nummber==2:
+                        break
+              elif incoming_data[0:11]=="NLIST BEGIN":
+                   break
+
 class ClientReadThread (threading.Thread):
     def __init__(self, name, cSocket,list_queue):
         threading.Thread.__init__(self)
